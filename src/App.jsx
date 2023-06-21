433abf164a8a8e42aaad700541524a04a20c03eb
digital opium <test@ismail.com>
2023-06-21 21:28:50 +0100

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './app.css';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
 
   return (
-    <div className='app'>
-     
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element='' >
+            <Route path='page1' element='' />
+            <Route path='page2' element='' />
+            <Route path='page3' element='' />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/Menu.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Menu = () => {
+    return (
+        <div className='app'>
+            
+        </div>
+    );
+}
+
+export default Menu;
